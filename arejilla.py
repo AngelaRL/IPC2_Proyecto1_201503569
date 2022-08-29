b5e414ef385fe840f65fba97517b19a90d65d209
Angela_Rodriguez <2659290670101@ingenieria.usac.edu.gt>
2022-08-28 23:31:52 -0600

    clases donde se encuentran los atributos de los pacientes

---
+++ arejilla.py
@@ -0,0 +1,7 @@
+class arejilla:
+
+    def __init__(self, f,c):
+
+        #f para filas y c para columna 
+        self.c = c
+        self.f = f
---
+++ paciente.py
@@ -0,0 +1,13 @@
+from arejilla import arejilla
+
+class paciente:
+
+    def __init__(self, nombre, edad, periodos, m, rejilla):
+        
+        #para almencenar los datos que contendra el archivo xml 
+        self.nombre = nombre
+        self.edad = edad
+        self.periodos = periodos
+        self.m = m
+        self.rejilla = rejilla
+
