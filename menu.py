eeb66c1e31c31a70a80719f5a3a7431a4a5c39ef
Angela_Rodriguez <2659290670101@ingenieria.usac.edu.gt>
2022-08-28 23:34:19 -0600

    se agregaron mas lineas al menu

--- menu.py
+++ menu.py
@@ -1,11 +1,13 @@
 from ast import Num
-
+from listaPacientes import listaPaciente
+import xml.etree.ElementTree as AE
 
 
 def menuprincipal():
 
     salir = False
     opcion = 0
+    subopcion =0
     while not salir:
         print("::::::::::::::::::::::::::::::::::: MENU :::::::::::::::::::::::::::::::::::")
         print("")
@@ -20,16 +22,48 @@ def menuprincipal():
         print("")
         print("")
         print("")
-        print("")
-        print("")
-        print("")
-        print("")
+    
 
         opcion = int(input("ingrese el numero"))
 
         if opcion == 1:
-            print("1")
+                       
+            print("")
+            ruta = input('Ingrese la ruta del archivo:')
+            print("")
+            print("")
+            ruta.insertar(ruta)
+
+            ruta.cargarPacientes()
+
+
+            
         elif opcion == 2:
+            while not salir:
+                print("")
+                print("")
+                print(":::::::::::___PACIENTE___::::::::::::")
+                print("")
+                print("1. Analizar Muestra Automaticamente")
+                print("2. Analizar Muestra por Periodo ")
+                print("3. Regresar al menu principal ")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+
+                subopcion = int(input("ingrese el numero de la opcion que desea:"))
+
+                if subopcion == 1:
+                    print("")
+                elif subopcion == 2:
+                    print("2")
+                elif subopcion == 3:
+                    menuprincipal()
+                else:
+                    print("opcion no valida")
+
             print("")
         elif opcion == 3:
             print("")
